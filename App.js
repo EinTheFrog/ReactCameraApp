5bb37e3fe6e18def5f97ac17a0d246e2f95efca4
Maxim Alpatski <MaximAlpatski@gmail.com>
2024-03-14 00:07:54 +0400

    Init

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  if (!permission) {
+    requestPermission()
+  }
+
+  if (permission && !permission.granted) {
+    return(
+      <View style={styles.container}>
+      <Text>No permission</Text>
+      </View>
+    )
+  } 
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} type={type}>
+      </Camera>
     </View>
   );
 }
@@ -14,7 +34,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  camera: {
+    flex: 1
+  },
+  buttonContainer: {
+    flex: 1,
+    alignContent: 'center'
+  }
 });
